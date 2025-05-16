5d2ecd4451de328b17101b79fcec21011df1c35c
elagamy <https://github.com/elagamy257>
2025-05-16 16:01:58 +0300

    change img

--- src/component/HomeSlider/HomeSlider.jsx
+++ src/component/HomeSlider/HomeSlider.jsx
@@ -1,9 +1,9 @@
-import slider1 from './../../assets/images/img-1.jpg';
-import slider2 from './../../assets/images/slider-image-1.jpeg';
-import slider3 from './../../assets/images/slider-image-3.jpeg';
-import slider4 from './../../assets/images/img-4.jpg';
-import img1 from './../../assets/images/slider-2.jpeg';
-import img2 from './../../assets/images/blog-img-1.jpeg';
+import slider1 from './../../assets/images/img2.jpg';
+import slider2 from './../../assets/images/img5.jpg';
+import slider3 from './../../assets/images/img7.jpg';
+import slider4 from './../../assets/images/img1.jpg';
+import img1 from './../../assets/images/slider1.jpg';
+import img2 from './../../assets/images/slider2.jpg';
 import Slider from "react-slick";
 
 function HomeSlider() {
@@ -35,23 +35,23 @@ function HomeSlider() {
           <div className="col-12 col-md-8 mb-4 mb-md-0">
             <Slider {...settings}>
               <div>
-                <img src={slider1} className="w-100 rounded" style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 1" />
+                <img src={slider1} className="w-100 " style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 1" />
               </div>
               <div>
-                <img src={slider2} className="w-100 rounded" style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 2" />
+                <img src={slider2} className="w-100 " style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 2" />
               </div>
               <div>
-                <img src={slider3} className="w-100 rounded" style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 3" />
+                <img src={slider3} className="w-100 " style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 3" />
               </div>
               <div>
-                <img src={slider4} className="w-100 rounded" style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 4" />
+                <img src={slider4} className="w-100 " style={{ height: '24rem', objectFit: 'cover' }} alt="Slider 4" />
               </div>
             </Slider>
           </div>
-          <div className="col-12 col-md-4">
+          <div className="col-12 col-md-4 ">
             <div className="d-flex flex-column">
-              <img src={img1} className="w-100 rounded " style={{ height: '12rem', objectFit: 'cover' }} alt="Banner 1" />
-              <img src={img2} className="w-100 rounded" style={{ height: '12rem', objectFit: 'cover' }} alt="Banner 2" />
+              <img src={img1} className="w-100  " style={{ height: '12rem', objectFit: 'cover' }} alt="Banner 1" />
+              <img src={img2} className="w-100 " style={{ height: '12rem', objectFit: 'cover' }} alt="Banner 2" />
             </div>
           </div>
         </div>
